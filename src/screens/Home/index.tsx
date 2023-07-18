297474b4f13fc925ed2ea07d1001593628267197
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2023-07-17 21:34:25 -0300

    feat; add flatList to improve performance

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
@@ -42,17 +42,23 @@ export function Home() {
                 </TouchableOpacity>
             </View>
 
-            <ScrollView showsVerticalScrollIndicator={false}>
-                {
-                    participants.map(participant => (
-                        <Participant
-                            key={participant} 
-                            name={participant}
+            <FlatList
+                data={participants}
+                keyExtractor={item => item}
+                renderItem={({ item }) =>(
+                    <Participant
+                            key={item} 
+                            name={item}
                             onRemove={() => handleParticipantRemove("Cybelle Vasconcelos")}
-                        />
-                    ))
-                }
-            </ScrollView>
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <Text style={styles.listEmptyText}>
+                        Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+                    </Text>
+                )}
+            />
             
         </View>
     );
